ce60a7d7737f77829e1f0b454bc1a3a024befb34
arfan36 <arfanur36@gmail.com>
2022-10-21 11:02:51 +0600

    60-3 Module Overview, Project setup and router setup

--- src/__step.js
+++ src/__step.js
@@ -1,4 +1,5 @@
 /* 
+//! ─── Basic Project Setup ─────────────────────────────────────────────────────
 1. create firebase app
 2. get firebase configuration inside firebase.config.js
 3. make sure to export app from firebase config
@@ -7,4 +8,9 @@
 6. install bootstrap and react-bootstrap 
 7. make sure to import bootstrap css in the index.js file
 
+ */
+
+/* 
+//! ─── Router Setup ────────────────────────────────────────────────────────────
+1. 
  */
