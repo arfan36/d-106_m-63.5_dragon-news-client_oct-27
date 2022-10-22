d3ea4095c8a76525c0a4760acfe91301f7daba8b
arfan36 <arfanur36@gmail.com>
2022-10-22 20:22:53 +0600

    61-5 Display News Details

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -2,27 +2,27 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
-import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
+import { FaEye, FaRegBookmark, FaShareAlt, FaStar } from 'react-icons/fa';
 
 const NewsSummaryCard = ({ news }) => {
-    const { _id, total_view, title, author, image_url, details } = news;
+    const { _id, total_view, title, author, image_url, details, rating } = news;
     return (
         <Card className="mb-5">
             <Card.Header className='d-flex justify-content-between align-items-center'>
                 <div className='d-flex'>
                     <Image
-                        className='me-2'
                         roundedCircle
-                        src={author.img}
+                        className='me-2'
+                        src={author?.img}
                         style={{ height: '60px' }}
                     ></Image>
                     <div>
-                        <p>{author.name}</p>
-                        <p>{author.published_date}</p>
+                        <p className='mb-0'>{author?.name}</p>
+                        <p>{author?.published_date}</p>
                     </div>
                 </div>
                 <div>
-                    <FaRegBookmark></FaRegBookmark>
+                    <FaRegBookmark className='me-2'></FaRegBookmark>
                     <FaShareAlt></FaShareAlt>
                 </div>
             </Card.Header>
@@ -32,13 +32,22 @@ const NewsSummaryCard = ({ news }) => {
                 <Card.Text>
                     {
                         details.length > 200 ?
-                            <p>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>read more</Link></p>
+                            <>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>read more</Link></>
                             :
-                            <p>{details}</p>
+                            <>{details}</>
                     }
                 </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted d-flex justify-content-between">
+                <div>
+                    <FaStar className='text-warning me-2'></FaStar>
+                    <span>{rating?.number}</span>
+                </div>
+                <div>
+                    <FaEye className='me-2'></FaEye>
+                    <span>{total_view}</span>
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
