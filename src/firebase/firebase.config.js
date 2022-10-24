32b2284286b89dcf9ecc030b32a414200478e4a5
arfan36 <arfanur36@gmail.com>
2022-10-24 10:59:00 +0600

    process.env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCYXY27d4-xRCEKFRNrCF44Vo1u2Kw9pMU",
-    authDomain: "dragon-news-d-101.firebaseapp.com",
-    projectId: "dragon-news-d-101",
-    storageBucket: "dragon-news-d-101.appspot.com",
-    messagingSenderId: "504673559565",
-    appId: "1:504673559565:web:c0b9e368040d21570f6144"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
