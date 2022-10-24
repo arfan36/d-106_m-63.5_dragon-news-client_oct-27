baf1f329ca46781d99414b10a38655ba5bc37b78
arfan36 <arfanur36@gmail.com>
2022-10-24 14:02:19 +0600

    62-9 Deploy Server and client side code with api url update [Previous Module]

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -5,7 +5,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/news-categories')
+        fetch('https://d-101-1-m-62-dragon-news-server-oct-20.vercel.app/news-categories')
             .then(res => res.json())
             .then(data => setCategories(data));
     }, []);
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -17,19 +17,19 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://d-101-1-m-62-dragon-news-server-oct-20.vercel.app/news')
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`)
+                loader: ({ params }) => fetch(`https://d-101-1-m-62-dragon-news-server-oct-20.vercel.app/category/${params.id}`)
             },
             {
                 path: '/news/:id',
                 element: <PrivateRoute>
                     <News></News>
                 </PrivateRoute>,
-                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({ params }) => fetch(`https://d-101-1-m-62-dragon-news-server-oct-20.vercel.app/news/${params.id}`)
             },
             {
                 path: '/login',
